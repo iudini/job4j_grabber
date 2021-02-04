ecff82b054b04e42ac823bf758257cfb74a90697
iudini <hoiitzx@gmail.com>
2021-02-04 23:37:54 +0300

    2. Парсинг HTML страницы. [#399914]

---
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -0,0 +1,20 @@
+package ru.job4j.html;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class SqlRuParse {
+    public static void main(String[] args) throws Exception {
+        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
+        Elements row = doc.select(".forumTable").select("tr");
+        for (int i = 1; i < row.size(); i++) {
+            Element href = row.get(i).child(1).tagName("a").child(0);
+            Element date = row.get(i).child(5);
+            System.out.println(href.attr("href"));
+            System.out.println(href.text());
+            System.out.println(date.text());
+        }
+    }
+}
