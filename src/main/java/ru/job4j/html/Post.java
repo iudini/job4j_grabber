4751539d4a4cc5794489ea11537b26f59595e54d
iudini <hoiitzx@gmail.com>
2021-02-05 18:52:02 +0300

    2.2. Модель данных - Post. [#399919]

---
+++ src/main/java/ru/job4j/html/Post.java
@@ -0,0 +1,49 @@
+package ru.job4j.html;
+
+import java.time.LocalDateTime;
+
+public class Post {
+    private String name;
+    private String desc;
+    private LocalDateTime dateTime;
+    private String url;
+
+    public Post(String name, String desc, LocalDateTime dateTime, String url) {
+        this.name = name;
+        this.desc = desc;
+        this.dateTime = dateTime;
+        this.url = url;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
