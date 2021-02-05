39ef2f77c5eb692a6ed33c6eb32647d18f016e8d
iudini <hoiitzx@gmail.com>
2021-02-05 18:35:39 +0300

    2.1.1. Парсинг https://www.sql.ru/forum/job-offers/3 [#399918]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -9,15 +9,17 @@ import java.time.LocalDateTime;
 
 public class SqlRuParse {
     public static void main(String[] args) throws Exception {
-        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
-        Elements row = doc.select(".forumTable").select("tr");
-        for (int i = 1; i < row.size(); i++) {
-            Element href = row.get(i).child(1).tagName("a").child(0);
-            Element date = row.get(i).child(5);
-            LocalDateTime dateTime = DateParse.parse(date.text());
-            System.out.println(href.attr("href"));
-            System.out.println(href.text());
-            System.out.println(dateTime);
+        for (int page = 1; page <= 5; page++) {
+            Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers/" + page).get();
+            Elements row = doc.select(".forumTable").select("tr");
+            for (int i = 1; i < row.size(); i++) {
+                Element href = row.get(i).child(1).tagName("a").child(0);
+                Element date = row.get(i).child(5);
+                LocalDateTime dateTime = DateParse.parse(date.text());
+                System.out.println(href.attr("href"));
+                System.out.println(href.text());
+                System.out.println(dateTime);
+            }
         }
     }
 }
