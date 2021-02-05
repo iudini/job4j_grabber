0728ebf488d5604d098b44e54ddd3494a61b2f84
iudini <hoiitzx@gmail.com>
2021-02-05 19:26:42 +0300

    2.3. Загрузка деталей поста. [#399920]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -5,6 +5,7 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.io.IOException;
 import java.time.LocalDateTime;
 
 public class SqlRuParse {
@@ -15,11 +16,18 @@ public class SqlRuParse {
             for (int i = 1; i < row.size(); i++) {
                 Element href = row.get(i).child(1).tagName("a").child(0);
                 Element date = row.get(i).child(5);
+                String url = href.attr("href");
+                String name = href.text();
                 LocalDateTime dateTime = DateParse.parse(date.text());
-                System.out.println(href.attr("href"));
-                System.out.println(href.text());
-                System.out.println(dateTime);
+                parsePostDesc(url, name, dateTime);
             }
         }
+    }
+
+    static Post parsePostDesc(String url, String name, LocalDateTime dateTime) throws IOException {
+        Document doc = Jsoup.connect(url).get();
+        Elements row = doc.select(".msgTable");
+        String desc = row.get(0).select(".msgBody").get(1).text();
+        return new Post(name, desc, dateTime, url);
     }
 }
