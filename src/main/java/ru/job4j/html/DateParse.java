446cc4cbd20ae0447a64c2c98ab40fae5a428994
iudini <hoiitzx@gmail.com>
2021-02-10 16:26:04 +0300

    2.1. Преобразование даты [#399922]

--- src/main/java/ru/job4j/html/DateParse.java
+++ src/main/java/ru/job4j/html/DateParse.java
@@ -7,30 +7,35 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class DateParse {
+    private static final DateTimeFormatter DATE_TIME_FORMAT =
+            DateTimeFormatter.ofPattern("dd MM yy, HH:mm");
+    private static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("dd MM yy");
+    private static final Map<String, String> MONTHS = monthsMap();
+
     public static LocalDateTime parse(String date) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MM yy, HH:mm");
-        Map<String, String> months = new HashMap<>();
-        months.putAll(Map.of("янв", "01", "фев", "02", "мар", "03", "апр", "04",
-                "май", "05", "июн", "06", "июл", "07", "авг", "08"));
-        months.putAll(Map.of("сен", "09", "окт", "10", "ноя", "11", "дек", "12"));
         if (date.contains("сегодня")) {
-            date = date.replace("сегодня",
-                    LocalDate.now().format(DateTimeFormatter.ofPattern("dd MM yy")).toString());
+            date = date.replace("сегодня", LocalDate.now().format(DATE_FORMAT));
         } else if (date.contains("вчера")) {
-            date = date.replace("вчера",
-                    LocalDate.now().minusDays(1)
-                            .format(DateTimeFormatter.ofPattern("dd MM yy")).toString());
+            date = date.replace("вчера", LocalDate.now().minusDays(1).format(DATE_FORMAT));
         } else {
             if (date.charAt(1) == ' ') {
                 date = "0" + date;
             }
-            for (var m : months.keySet()) {
+            for (var m : MONTHS.keySet()) {
                 if (date.contains(m)) {
-                    date = date.replace(m, months.get(m));
+                    date = date.replace(m, MONTHS.get(m));
                     break;
                 }
             }
         }
-        return LocalDateTime.parse(date, formatter);
+        return LocalDateTime.parse(date, DATE_TIME_FORMAT);
+    }
+
+    private static Map<String, String> monthsMap() {
+        Map<String, String> months = new HashMap<>();
+        months.putAll(Map.of("янв", "01", "фев", "02", "мар", "03", "апр", "04",
+                "май", "05", "июн", "06", "июл", "07", "авг", "08"));
+        months.putAll(Map.of("сен", "09", "окт", "10", "ноя", "11", "дек", "12"));
+        return months;
     }
 }
